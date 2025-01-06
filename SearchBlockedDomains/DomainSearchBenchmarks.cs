99f90e63ed5ef078920474cd7ad7ca9be66708c7
Anil.Poonia <anil.poonia@inboxinsight.com>
2025-01-06 15:55:16 +0000

    Add benchmark to know code performance.

---
+++ SearchBlockedDomains/DomainSearchBenchmarks.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+using BenchmarkDotNet.Attributes;
+using SearchBlockedDomains;
+
+[MemoryDiagnoser]
+public class DomainSearchBenchmarks
+{
+    //"BlockedDomains.txt" have all domains that are available in "AvailableDomains.txt" except google.com, mail.google.com, maps.google.com, onedrive.google.com, etc.
+    private readonly string[] availableDomains = File.ReadAllLines(@"AvailableDomains.txt");
+    private readonly string[] blockedDomains = File.ReadAllLines(@"BlockedDomains.txt");
+
+    [Benchmark(Baseline = true)]
+    public void ListIndexesOfDomainsThatCanBeVisited()
+    {
+        var domainSearch = new DomainSearch();
+
+        domainSearch.ListIndexesOfDomainsThatCanBeVisited(availableDomains, blockedDomains);
+    }
+}
--- SearchBlockedDomains/Program.cs
+++ SearchBlockedDomains/Program.cs
@@ -1,2 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
+//using BenchmarkDotNet.Running;
+using SearchBlockedDomains;
+
 Console.WriteLine("Search Blocked Domains");
+
+var availableDomains = new[] { "unlock.microservice.md", "visitwar.com", "visitwar.de", "fruonline.co.uk", "australia.open.com", "credit.card.us" };
+var blockedDomains = new[] { "microservice.md", "visitwar.de", "piratebay.co.uk", "list.stolen.credit.card.us" };
+
+var domainSearch = new DomainSearch();
+var result = domainSearch.ListIndexesOfDomainsThatCanBeVisited(availableDomains, blockedDomains);
+Console.WriteLine(String.Join(",", result));
+
+//In order to view benchmark result, Please follow these steps:
+//1. Change "Debug" to "Release"
+//2. Uncomment line number 2.
+//3. Uncomment below line 
+
+//var summary = BenchmarkRunner.Run<DomainSearchBenchmarks>();
